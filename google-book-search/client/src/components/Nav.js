798ea425765ab81d18e2f2edddc01c76853b0289
Jacqueline Gwynn <jacquelinegwynn@jacquelines-air.attlocal.net>
2019-05-05 15:44:30 -0400

    per page design added header to page

--- google-book-search/client/src/components/Nav.js
+++ google-book-search/client/src/components/Nav.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"; 
 
 function Nav() {
   return (
     <nav>
+      <h4>Google Books</h4>
       <Link to="/">Search</Link>
-      <br></br>
       <Link to="/saved">Saved</Link>
     </nav>
   );
