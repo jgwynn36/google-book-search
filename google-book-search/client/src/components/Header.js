bf7ba3f4a0dbce12febca800900f0a24a42bd60f
Jacqueline Gwynn <jacquelinegwynn@jacquelines-air.attlocal.net>
2019-05-05 15:45:40 -0400

    update header to container and css for formatting Search page

--- google-book-search/client/src/components/Header.js
+++ google-book-search/client/src/components/Header.js
@@ -1,27 +1,20 @@
 import React from "react";
-import heroImage from "../images/bookimage.jpeg";
-
-const headerStyle = {
-  backgroundImage: `url(${heroImage})`,
-  maxWidth: "100vw",
-  height: "calc(100vw * .43333)",
-  backgroundSize: "cover",
-  position: "relative"
-};
 
 function Header() {
   return (
-    <header style={headerStyle}>
-      <h1>Google Books Search</h1>
-      <p>Search for and Save Books of Interest</p>
-      <blockquote>
-        <p>
-          "I know nothing with any certainty, but the sight of the stars makes
-          m dream."
-        </p>
-        <footer>-- Vincent Van Gogh </footer>
-      </blockquote>
-    </header>
+    <div className="container-fluid">
+      <header>
+        <h1>(React) Google Books Search</h1>
+        <h2>Search for and Save Books of Interest</h2>
+        <blockquote>
+          <p>
+            "I know nothing with any certainty, but the sight of the stars
+            makes m dream."
+          </p>
+          <footer>-- Vincent Van Gogh </footer>
+        </blockquote>
+      </header>
+    </div>
   );
 }
 
