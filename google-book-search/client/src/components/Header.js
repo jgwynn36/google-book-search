fcbe7b0d9ad2647444d8e2c3f6c5762a828f5c56
Jacqueline Gwynn <jacquelinegwynn@jacquelines-air.attlocal.net>
2019-05-05 01:45:34 -0400

    update Header file to include new image and display correctly

--- google-book-search/client/src/components/Header.js
+++ google-book-search/client/src/components/Header.js
@@ -1,19 +1,26 @@
 import React from "react";
-// import heroImage from "../images/books-2.jpg";
+import heroImage from "../images/bookimage.jpeg";
 
-// const headerStyle = {
-//   backgroundImage: `url(${heroImage})`,
-//   maxWidth: "100vw",
-//   height: "calc(100vw * .43333)",
-//   backgroundSize: "cover",
-//   position: "relative"
-// };
+const headerStyle = {
+  backgroundImage: `url(${heroImage})`,
+  maxWidth: "100vw",
+  height: "calc(100vw * .43333)",
+  backgroundSize: "cover",
+  position: "relative"
+};
 
 function Header() {
   return (
-    <header>
-      <h1>(React) Google Books Search</h1>
+    <header style={headerStyle}>
+      <h1>Google Books Search</h1>
       <p>Search for and Save Books of Interest</p>
+      <blockquote>
+        <p>
+          "I know nothing with any certainty, but the sight of the stars makes
+          m dream."
+        </p>
+        <footer>-- Vincent Van Gogh </footer>
+      </blockquote>
     </header>
   );
 }
