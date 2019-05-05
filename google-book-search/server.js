49ec3fc365cba619cd65891555d24b3d9f89b051
Jacqueline Gwynn <jacquelinegwynn@jacquelines-air.attlocal.net>
2019-05-05 01:22:15 -0400

    add npm package axios deleted DB seed files

--- google-book-search/server.js
+++ google-book-search/server.js
@@ -11,13 +11,26 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+const mongoose = require("mongoose");
+const mongoURL =
+  process.env.PROD_MONGODB || "mongodb://localhost:27017/googlebooks";
+mongoose
+  .connect(mongoURL, { useNewUrlParser: true })
+  .then(() => {
+    console.log("🗄 ==> Successfully connected to mongoDB.");
+  })
+  .catch(err => {
+    console.log(`Error connecting to mongoDB: ${err}`);
+  });
+
 // Define API routes here
+require("./routes/api/api")(app);
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
